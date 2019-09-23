bfcb2399098773d30b9a6613755e11152e7b4a77
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-23 13:03:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     self.each do |word|
-      
+
     if self.include?(".") && self.include?("?") && self.include?("!")
       (self.split(".") && self.split("!") && self.split("?")).length
     end
